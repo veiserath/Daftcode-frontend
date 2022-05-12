b69c4431a9313f9b5d4afd58a78abc4bff4d4586
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-05-12 12:33:16 +0200

    deleted edit src/App.js paragraph

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <button onClick={handleButtonClicked}>Greetings!</button>
         <a
           className="App-link"
