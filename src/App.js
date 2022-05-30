92d664c93b25db221257a4627e995928e33e6212
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-05-30 16:16:02 +0200

    Before mobile version added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './login';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from './pages/index'
+import About from './pages/about';
+import Navbar from './navbar';
+import ContactUs from './pages/contactus';
+import SignUp from './pages/signup';
 
 function App() {
   const handleButtonClicked = () => {
     alert('Hello from React!')
   }
 
+  const LoginPage = () => {
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={handleButtonClicked}>Greetings!</button>
+          <a
+            className="App-link"
+            href="https://github.com/veiserath/Daftcode-frontend"
+            target="_blank"
+            rel="noopener noreferrer">
+            Link to GitHub repo
+          </a>
+          <Login onChange={handleButtonClicked}></Login>
+        </header>
+      </div>
+    );
+  };
+
+
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={handleButtonClicked}>Greetings!</button>
-        <a
-          className="App-link"
-          href="https://github.com/veiserath/Daftcode-frontend"
-          target="_blank"
-          rel="noopener noreferrer">
-          Link to GitHub repo
-        </a>
-        
-        <Login onChange={handleButtonClicked}></Login>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <Route index path='/' element={<LoginPage />} />
+        <Route path='/about' element={<AboutPage />} /> */}
+
+      <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/signin' element={<LoginPage />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+import logo from './logo.svg'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img src={logo} alt='logo' />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
