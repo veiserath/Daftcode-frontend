e05022fda25afa53d56dceebdd696ef51c41732b
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-05-31 08:37:35 +0200

    Refactored names

--- src/pages/AboutUsSection/index.js
+++ src/pages/AboutUsSection/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Video from './video.mp4'
-import { AboutUsContainer, AboutUsBg, VideoBg } from './AboutUsElements';
+import { AboutUsContainer, AboutUsBg as AboutUsBackground, VideoBg as VideoBackground } from './AboutUsElements';
 
 const AboutUsSection = () => {
   return (
     <AboutUsContainer>
-        <AboutUsBg>
-            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-        </AboutUsBg>
+        <AboutUsBackground>
+            <VideoBackground autoPlay loop muted src={Video} type='video/mp4' />
+        </AboutUsBackground>
     </AboutUsContainer>
   );
 };
