b009d3f0ffceccf9e3886683a2d53bf99cf96af8
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-06-04 20:20:26 +0200

    working API hook

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const ContactUs = () => {
+export default function ContactUs() {
+  const [data, setData] = useState({ results: [] });
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(query);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        `https://rickandmortyapi.com/api/character/?name=${query}`
+      );
+      setData(result.data);
+    }
+    fetchData();
+  }, [search]);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Contact Us</h1>
+    <div className="resultsWrapper">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+      <div className="results">
+        <ul>
+          {data.results.map((item) => (
+            <li key={item.id}>
+              <img alt={item.name} src={item.image} />
+
+              <span className="name">{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-};
-
-export default ContactUs;
+}
