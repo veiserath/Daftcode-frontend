53dacbcfc0417aeef1cc6758192eb1569f225b63
Kacper Mlodkowski <k.mlodkowski@gmail.com>
2022-06-05 23:45:02 +0200

    Fixed imports

--- src/pages/rickandmorty.js
+++ src/pages/rickandmorty.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from 'styled-components';
-import { Input, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 
 
 const StyledButton = styled.button`
